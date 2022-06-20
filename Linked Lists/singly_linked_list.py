f4dda48e33be10663c7abd3b68d1beee09f57c84
Sajib Mandal <107785806+sajib-mandal@users.noreply.github.com>
2022-06-20 22:28:04 +0530

    Create singly_linked_list.py

---
+++ Linked Lists/singly_linked_list.py
@@ -0,0 +1,21 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+
+    # inserting the node at the beginning
+
+    def insertAtStart(self, data):
+        if self.head == None:
+            newNode = Node(data)
+            self.head = newNode
+        else:
+            newNode = Node(data)
+            newNode.next = self.head
+            self.head = newNode
