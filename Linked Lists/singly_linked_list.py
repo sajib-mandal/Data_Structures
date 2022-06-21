7789e0d7383b3f4e0f9041796f1bf61094ebf847
Sajib Mandal <107785806+sajib-mandal@users.noreply.github.com>
2022-06-21 14:53:12 +0530

    Update singly_linked_list.py

--- Linked Lists/singly_linked_list.py
+++ Linked Lists/singly_linked_list.py
@@ -3,10 +3,29 @@ class Node:
         self.data = data
         self.next = None
 
+    def setData(self, data):
+        self.data = data
+
+    def getData(self):
+        return self.data
+
+    def setNext(self, next):
+        self.next = next
+
+    def getNext(self):
+        return self.next
 
 class LinkedList:
+    # defining the head of the linked list
     def __init__(self):
         self.head = None
+
+    # print the data in the linked list
+    def printLinkedList(self):
+        temp = self.head
+        while temp:
+            print(temp.data, end= ' ')
+            temp = temp.next
 
     # inserting the "Node" at the first
 
@@ -38,14 +57,7 @@ class LinkedList:
             newNode.next = previousNode.next
             previousNode.next = newNode
 
-    # searching an item in the list
 
-    def search(self, node, data):
-        if node == None:
-            return False
-        if node.data == data:
-            return True
-        return self.serach(node.getNext(), data)
 
 
     # delete an item in the list
@@ -72,3 +84,29 @@ class LinkedList:
                 return
             prev.next = temp.next
             return
+
+        # searching an item in the list
+
+        def search(self, node, data):
+            if node == None:
+                return False
+            if node.data == data:
+                return True
+            return self.search(node.getNext(), data)
+
+if __name__ == '__main__':
+    list = LinkedList()
+    list.head = Node(1)
+    node2 = Node(2)
+    list.head.setNext(node2)
+    node3 = Node(3)
+    node2.setNext(node3)
+    list.insertAtStart(4)
+    list.insertBetween(node2, 5)
+    list.insertAtEnd(6)
+    list.printLinkedList()
+    print()
+    list.delete(3)
+    list.printLinkedList()
+    print()
+    print(list.search(list.head, 1))
