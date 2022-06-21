eca3ec3e8ad77ed36f1d81f9ad054ac36fea111e
Sajib Mandal <107785806+sajib-mandal@users.noreply.github.com>
2022-06-21 18:34:18 +0530

    Update doubly_linked_lists.py

--- Linked Lists/doubly_linked_lists.py
+++ Linked Lists/doubly_linked_lists.py
@@ -1,6 +1,3 @@
-import sys
-
-
 class Node:
     def __init__(self, data):
         self.data = data
@@ -44,8 +41,7 @@ class MyLinkedList:
             curr = curr.next
 
         if curr == None:
-            print
-            'Key not found'
+            print("Key not found")
             return
 
         if curr.next == None:
@@ -62,8 +58,7 @@ class MyLinkedList:
     # returns the data at first node
     def top_front(self):
         if self.is_empty():
-            print
-            'List is empty'
+            print("List is empty")
             return
 
         return self.head.data
@@ -71,8 +66,7 @@ class MyLinkedList:
     # returns the data at last node
     def top_back(self):
         if self.is_empty():
-            print
-            'List is empty'
+            print("List is empty")
             return
 
         curr = self.head
@@ -84,8 +78,7 @@ class MyLinkedList:
     # removes the item at front of the linked list and return
     def pop_front(self):
         if self.is_empty():
-            print
-            'List is empty'
+            print("List is empty")
             return
 
         next_node = self.head.next
@@ -98,8 +91,7 @@ class MyLinkedList:
     # remove the item at the end of the list and return
     def pop_back(self):
         if self.is_empty():
-            print
-            'List is empty'
+            print("List is empty")
             return
 
         item = self.tail.data
@@ -110,14 +102,12 @@ class MyLinkedList:
 
         self.tail.prev = None
         self.tail = prev
-
         return item
 
     # removes an item with value 'key'
     def remove(self, key):
         if self.is_empty():
-            print
-            'List is empty'
+            print("List is empty")
             return
 
         # find the position of the key
@@ -127,8 +117,7 @@ class MyLinkedList:
             curr = curr.next
 
         if curr == None:
-            print
-            'key not found'
+            print("Key not found")
             return
 
         # if curr is head, delete the head
@@ -150,8 +139,7 @@ class MyLinkedList:
     # check if the key is in the list
     def find(self, key):
         if self.is_empty():
-            print
-            'List is empty'
+            print("List is empty")
             return False
 
         curr = self.head
@@ -166,3 +154,31 @@ class MyLinkedList:
     # check if the list is empty
     def is_empty(self):
         return self.head == None
+
+
+    def printLinkedList(self):
+        temp = self.head
+        while temp:
+            print(temp.data, end=' ')
+            temp = temp.next
+
+if __name__ == '__main__':
+
+    list = MyLinkedList()
+    list.insert_at_front(44)
+    list.insert_at_front(66)
+    list.insert_at_back(21)
+    list.insert_at_back(43)
+    list.insert_after(43, 49)
+    list.insert_after(21, 33)
+    list.printLinkedList()
+    print()
+    list.pop_front()
+    list.printLinkedList()
+    print()
+    list.pop_back()
+    list.printLinkedList()
+    print()
+    list.remove(21)
+    list.printLinkedList()
+    print()
