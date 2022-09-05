ed272a30ab1d4675264f3d5b600d28b598c2a25f
Sajib Mandal <107785806+sajib-mandal@users.noreply.github.com>
2022-09-05 18:31:59 +0530

    Update zipper_list.py

--- Linked Lists/zipper_list.py
+++ Linked Lists/zipper_list.py
@@ -78,3 +78,55 @@ print(display())
 
 # recursive zipper list
 
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+
+
+def zipper_lists(head_1, head_2):
+    if head_1 is None and head_2 is None:
+        return None
+    if head_1 is None:
+        return head_2
+    if head_2 is None:
+        return head_1
+
+    next_1 = head_1.next
+    next_2 = head_2.next
+    head_1.next = head_2
+    head_2.next = zipper_lists(next_1, next_2)
+    return head_1
+
+
+def display():
+    tail = zipper_lists(a, x)
+    while tail is not None:
+        print(tail.val, end="->")
+        tail = tail.next
+
+    print("NULL")
+
+
+a = Node("a")
+b = Node("b")
+c = Node("c")
+d = Node("d")
+e = Node("e")
+f = Node("f")
+a.next = b
+b.next = c
+c.next = d
+d.next = e
+e.next = f
+# a -> b -> c -> d -> e -> f
+
+x = Node("x")
+y = Node("y")
+z = Node("z")
+x.next = y
+y.next = z
+# x -> y -> z
+
+print(display())
+# a -> x -> b -> y -> c -> z -> d -> e -> f
