f0ab7c1e3b9bdd7998798ed9993eb67d5a783e10
Sajib Mandal <107785806+sajib-mandal@users.noreply.github.com>
2022-09-05 15:44:01 +0530

    Update linked_list_find.py

--- Linked Lists/linked_list_find.py
+++ Linked Lists/linked_list_find.py
@@ -59,3 +59,59 @@ node1 = Node(42)
 # 42
 
 print(linked_list_find(node1, 100))  # False
+
+
+
+# recursive linked list find problem
+
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+
+
+def linked_list_find(head, target):
+    if head is None:
+        return False
+    if head.val == target:
+        return True
+    return linked_list_find(head.next, target)
+
+
+a = Node("a")
+b = Node("b")
+c = Node("c")
+d = Node("d")
+
+a.next = b
+b.next = c
+c.next = d
+
+# a -> b -> c -> d
+
+print(linked_list_find(a, "c"))  # True
+
+print(linked_list_find(a, "d"))  # True
+
+print(linked_list_find(a, "q"))  # False
+
+node1 = Node("jason")
+node2 = Node("leneli")
+
+node1.next = node2
+
+# jason -> leneli
+
+print(linked_list_find(node1, "jason"))  # True
+
+node1 = Node(42)
+
+# 42
+
+print(linked_list_find(node1, 42))  # True
+
+node1 = Node(42)
+
+# 42
+
+print(linked_list_find(node1, 100))  # False
