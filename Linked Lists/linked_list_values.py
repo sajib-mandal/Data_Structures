9ea492421e171965de6eabab893d1d798cd8f84f
Sajib Mandal <107785806+sajib-mandal@users.noreply.github.com>
2022-09-05 12:38:17 +0530

    Update linked_list_values.py

--- Linked Lists/linked_list_values.py
+++ Linked Lists/linked_list_values.py
@@ -53,3 +53,56 @@ q = Node("q")
 print(linked_list_values(q))  # -> [ 'q' ]
 
 print(linked_list_values(None))  # -> [ ]
+
+
+# recursive linked list values
+
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+
+
+def linked_list_values(head):
+    values = []
+    fill_values(head, values)
+
+    return values
+
+
+def fill_values(head, values):
+    if head is None:
+        return
+    values.append(head.val)
+    fill_values(head.next, values)
+
+
+a = Node("a")
+b = Node("b")
+c = Node("c")
+d = Node("d")
+
+a.next = b
+b.next = c
+c.next = d
+
+# a -> b -> c -> d
+
+print(linked_list_values(a))  # -> [ 'a', 'b', 'c', 'd' ]
+
+x = Node("x")
+y = Node("y")
+
+x.next = y
+
+# x -> y
+
+print(linked_list_values(x))  # -> [ 'x', 'y' ]
+
+q = Node("q")
+
+# q
+
+print(linked_list_values(q))  # -> [ 'q' ]
+
+print(linked_list_values(None))  # -> [ ]
