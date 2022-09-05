1df1041f9181fc47072b2f44b359024815d08110
Sajib Mandal <107785806+sajib-mandal@users.noreply.github.com>
2022-09-05 16:44:06 +0530

    Update reverse_list.py

--- Linked Lists/reverse_list.py
+++ Linked Lists/reverse_list.py
@@ -60,3 +60,50 @@ print(reverse_list(p))  # p
 
 
 # recursive reverse list with return new head value
+
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+
+
+def reverse_list(head, prev = None):
+    if head is None:
+        return prev.val
+    next = head.next
+    head.next = prev
+    return reverse_list(next, head)
+
+
+
+a = Node("a")
+b = Node("b")
+c = Node("c")
+d = Node("d")
+e = Node("e")
+f = Node("f")
+
+a.next = b
+b.next = c
+c.next = d
+d.next = e
+e.next = f
+
+# a -> b -> c -> d -> e -> f
+
+print(reverse_list(a))  # f -> e -> d -> c -> b -> a
+
+x = Node("x")
+y = Node("y")
+
+x.next = y
+
+# x -> y
+
+print(reverse_list(x))  # y -> x
+
+p = Node("p")
+
+# p
+
+print(reverse_list(p))  # p
