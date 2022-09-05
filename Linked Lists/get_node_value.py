7b6432afb10e74ba9d6043bf26538c8c3b189c8a
Sajib Mandal <107785806+sajib-mandal@users.noreply.github.com>
2022-09-05 16:12:06 +0530

    Update get_node_value.py

--- Linked Lists/get_node_value.py
+++ Linked Lists/get_node_value.py
@@ -6,6 +6,50 @@ list at the specified index.
 If there is no node at the given index, then return None.
 """
 
+# iterative get_node_value linked list
+
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+
+
+def get_node_value(head, index):
+    current = head
+    count = 0
+    while current is not None:
+        if count == index:
+            return current.val
+        count += 1
+        current = current.next
+    return None
+
+
+a = Node("a")
+b = Node("b")
+c = Node("c")
+d = Node("d")
+
+a.next = b
+b.next = c
+c.next = d
+
+# a -> b -> c -> d
+
+print(get_node_value(a, 2))  # 'c'
+print(get_node_value(a, 3))  # 'd'
+print(get_node_value(a, 7))  # None
+
+node1 = Node("banana")
+node2 = Node("mango")
+
+node1.next = node2
+
+# banana -> mango
+
+print(get_node_value(node1, 0))  # 'banana'
+print(get_node_value(node1, 1))  # 'mango'
+
 
 # recursive get_node_value linked list
 
