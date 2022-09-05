971a4bd2693a34a04bc555ca3f24914a503f3445
Sajib Mandal <107785806+sajib-mandal@users.noreply.github.com>
2022-09-05 13:05:05 +0530

    Update sum_list.py

--- Linked Lists/sum_list.py
+++ Linked Lists/sum_list.py
@@ -36,3 +36,50 @@ d.next = e
 # 2 -> 8 -> 3 -> -1 -> 7
 
 print(sum_list(a))  # 19
+
+
+# recursive linked list total_sum
+
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+
+
+def sum_list(head):
+    if head is None:
+        return 0
+    return head.val + sum_list(head.next)
+
+
+a = Node(2)
+b = Node(8)
+c = Node(3)
+d = Node(-1)
+e = Node(7)
+
+a.next = b
+b.next = c
+c.next = d
+d.next = e
+
+# 2 -> 8 -> 3 -> -1 -> 7
+
+print(sum_list(a))  # 19
+
+x = Node(38)
+y = Node(4)
+
+x.next = y
+
+# 38 -> 4
+
+print(sum_list(x))  # 42
+
+z = Node(100)
+
+# 100
+
+print(sum_list(z))  # 100
+
+print(sum_list(None))  # 0
