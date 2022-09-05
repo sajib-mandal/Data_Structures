4722626a75c8957057af93a1a25124ebf20e213b
Sajib Mandal <107785806+sajib-mandal@users.noreply.github.com>
2022-09-05 18:19:43 +0530

    Create zipper_list.py

---
+++ Linked Lists/zipper_list.py
@@ -0,0 +1,80 @@
+"""
+Zipper Lists:
+Write a function, zipper_lists, that takes in the head of two linked
+lists as arguments. The function should zipper the two lists together
+into single linked list by alternating nodes. If one of the linked lists
+is longer than the other, the resulting list should terminate with the
+remaining nodes. The function should return the head of the zippered linked list.
+
+Do this in-place, by mutating the original Nodes.
+
+You may assume that both input lists are non-empty.
+"""
+
+# iterative zipper list
+
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+
+
+def zipper_lists(head_1, head_2):
+    tail = head_1
+    current_1 = head_1.next
+    current_2 = head_2
+    count = 0
+
+    while current_1 is not None and current_2 is not None:
+        if count % 2 == 0:
+            tail.next = current_2
+            current_2 = current_2.next
+        else:
+            tail.next = current_1
+            current_1 = current_1.next
+        tail = tail.next
+        count += 1
+
+    if current_1 is not None:
+        tail.next = current_1
+    if current_2 is not None:
+        tail.next = current_2
+
+    return head_1
+
+
+def display():
+    tail = zipper_lists(a, x)
+    while tail is not None:
+        print(tail.val, end="->")
+        tail = tail.next
+
+    print("NULL")
+
+
+a = Node("a")
+b = Node("b")
+c = Node("c")
+d = Node("d")
+e = Node("e")
+f = Node("f")
+a.next = b
+b.next = c
+c.next = d
+d.next = e
+e.next = f
+# a -> b -> c -> d -> e -> f
+
+x = Node("x")
+y = Node("y")
+z = Node("z")
+x.next = y
+y.next = z
+# x -> y -> z
+
+print(display())
+# a -> x -> b -> y -> c -> z -> d -> e -> f
+
+
+# recursive zipper list
+
