a71d2b979b97e7d0ed8dbf8447bb7878a1e875b0
Sajib Mandal <107785806+sajib-mandal@users.noreply.github.com>
2022-09-06 18:18:25 +0530

    Update tree_min_value.py

--- Binary Tree/tree_min_value.py
+++ Binary Tree/tree_min_value.py
@@ -57,3 +57,55 @@ print(tree_min_value(a))  # -> -2
 
 
 # iterative breadth_first_traversal
+
+
+from collections import deque
+
+
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+
+
+def tree_min_value(root):
+    smallest = float("inf")
+
+    queue = deque([root])
+    while queue:
+        current = queue.popleft()
+        if current.val < smallest:
+            smallest = current.val
+
+        if current.right is not None:
+            queue.append(current.right)
+
+        if current.left is not None:
+            queue.append(current.left)
+
+    return smallest
+
+
+a = Node(3)
+b = Node(11)
+c = Node(4)
+d = Node(4)
+e = Node(-2)
+f = Node(1)
+
+a.left = b
+a.right = c
+b.left = d
+b.right = e
+c.right = f
+
+#       3
+#    /    \
+#   11     4
+#  / \      \
+# 4   -2     1
+print(tree_min_value(a))  # -> -2
+
+
+# recursive depth_first_search
