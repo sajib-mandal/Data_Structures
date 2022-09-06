9ae492df1fc400901f0538c14953a850ebfa2a50
Sajib Mandal <107785806+sajib-mandal@users.noreply.github.com>
2022-09-06 14:15:09 +0530

    Update breadth_first_search.py

--- Binary Tree/breadth_first_search.py
+++ Binary Tree/breadth_first_search.py
@@ -6,6 +6,7 @@ The function should return a list containing all values of the tree in breadth-f
 
 # iterative version
 
+# n2 time 
 class Node:
     def __init__(self, val):
         self.val = val
@@ -53,3 +54,60 @@ c.right = f
 
 print(breadth_first_values(a))
 #    -> ['a', 'b', 'c', 'd', 'e', 'f']
+
+
+# n time using dqueue
+
+from collections import deque
+
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.right = None
+        self.left = None
+
+
+def breadth_first_values(root):
+    if root is None:
+        return []
+
+    values = []
+    queue = deque([root])
+
+    while queue:
+        current = queue.popleft()
+        values.append(current.val)
+
+        if current.left is not None:
+            queue.append(current.left)
+
+        if current.right is not None:
+            queue.append(current.right)
+    return values
+
+
+a = Node('a')
+b = Node('b')
+c = Node('c')
+d = Node('d')
+e = Node('e')
+f = Node('f')
+
+a.left = b
+a.right = c
+b.left = d
+b.right = e
+c.right = f
+
+#      a
+#    /   \
+#   b     c
+#  / \     \
+# d   e     f
+
+print(breadth_first_values(a))
+#    -> ['a', 'b', 'c', 'd', 'e', 'f']
+
+
+
+# recursive version
