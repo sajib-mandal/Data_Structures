dae1c474bd3947001b5ac56eb0e92cfb0ac2196e
Sajib Mandal <107785806+sajib-mandal@users.noreply.github.com>
2022-09-06 16:24:25 +0530

    Update tree_includes.py

--- Binary Tree/tree_includes.py
+++ Binary Tree/tree_includes.py
@@ -62,3 +62,58 @@ print(tree_includes(a, "n"))  # -> False
 
 # iterative depth_first_search
 
+
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+
+
+def tree_includes(root, target):
+    if root is None:
+        return False
+
+    stack = [root]
+
+    while stack:
+        current = stack.pop()
+
+        if current.val == target:
+            return True
+
+        if current.right is not None:
+            stack.append(current.right)
+
+        if current.left is not None:
+            stack.append(current.left)
+
+    return False
+
+
+a = Node("a")
+b = Node("b")
+c = Node("c")
+d = Node("d")
+e = Node("e")
+f = Node("f")
+
+a.left = b
+a.right = c
+b.left = d
+b.right = e
+c.right = f
+
+#      a
+#    /   \
+#   b     c
+#  / \     \
+# d   e     f
+
+print(tree_includes(a, "e"))  # -> True
+print(tree_includes(a, "a"))  # -> True
+print(tree_includes(a, "n"))  # -> False
+
+
+# recursive depth first search
+
