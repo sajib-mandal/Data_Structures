486e9405255271dd79d394ff40fa51b6689b1a34
Sajib Mandal <107785806+sajib-mandal@users.noreply.github.com>
2022-09-06 16:41:35 +0530

    Update tree_includes.py

--- Binary Tree/tree_includes.py
+++ Binary Tree/tree_includes.py
@@ -117,3 +117,73 @@ print(tree_includes(a, "n"))  # -> False
 
 # recursive depth first search
 
+
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+
+
+def tree_includes(root, target):
+    if root is None:
+        return False
+
+    if root.val == target:
+        return True
+
+    return tree_includes(root.left, target) or tree_includes(root.right, target)
+
+
+a = Node("a")
+b = Node("b")
+c = Node("c")
+d = Node("d")
+e = Node("e")
+f = Node("f")
+
+a.left = b
+a.right = c
+b.left = d
+b.right = e
+c.right = f
+
+#      a
+#    /   \
+#   b     c
+#  / \     \
+# d   e     f
+
+print(tree_includes(a, "e"))  # -> True
+print(tree_includes(a, "a"))  # -> True
+print(tree_includes(a, "n"))  # -> False
+
+a = Node("a")
+b = Node("b")
+c = Node("c")
+d = Node("d")
+e = Node("e")
+f = Node("f")
+g = Node("g")
+h = Node("h")
+
+a.left = b
+a.right = c
+b.left = d
+b.right = e
+c.right = f
+e.left = g
+f.right = h
+
+#      a
+#    /   \
+#   b     c
+#  / \     \
+# d   e     f
+#    /       \
+#   g         h
+
+print(tree_includes(a, "f"))  # -> True
+print(tree_includes(a, "p"))  # -> False
+
+print(tree_includes(None, "b"))  # -> False
