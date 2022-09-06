3ebdcdff71ade8993c3214fdbcac4582d154662f
Sajib Mandal <107785806+sajib-mandal@users.noreply.github.com>
2022-09-06 11:45:17 +0530

    Update depth_first_values.py

--- Binary Tree/depth_first_values.py
+++ Binary Tree/depth_first_values.py
@@ -59,3 +59,95 @@ print(depth_first_values(a))
 
 # recursive version
 
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.right = None
+        self.left = None
+
+
+def depth_first_values(root):
+    if root is None:
+        return []
+    left_values = depth_first_values(root.left)
+    right_values = depth_first_values(root.right)
+    # return [root.val, *left_values, *right_values]
+    return [root.val] + left_values + right_values      # both return value is work correctly
+
+
+a = Node('a')
+b = Node('b')
+c = Node('c')
+d = Node('d')
+e = Node('e')
+f = Node('f')
+a.left = b
+a.right = c
+b.left = d
+b.right = e
+c.right = f
+
+#      a
+#    /   \
+#   b     c
+#  / \     \
+# d   e     f
+
+print(depth_first_values(a))
+#   -> ['a', 'b', 'd', 'e', 'c', 'f']
+
+a = Node('a')
+b = Node('b')
+c = Node('c')
+d = Node('d')
+e = Node('e')
+f = Node('f')
+g = Node('g')
+a.left = b
+a.right = c
+b.left = d
+b.right = e
+c.right = f
+e.left = g
+
+#      a
+#    /   \
+#   b     c
+#  / \     \
+# d   e     f
+#    /
+#   g
+
+print(depth_first_values(a))
+#   -> ['a', 'b', 'd', 'e', 'g', 'c', 'f']
+
+a = Node('a')
+#     a
+print(depth_first_values(a))
+#   -> ['a']
+
+a = Node('a')
+b = Node('b')
+c = Node('c')
+d = Node('d')
+e = Node('e')
+a.right = b
+b.left = c
+c.right = d
+d.right = e
+
+#      a
+#       \
+#        b
+#       /
+#      c
+#       \
+#        d
+#         \
+#          e
+
+print(depth_first_values(a))
+#   -> ['a', 'b', 'c', 'd', 'e']
+
+print(depth_first_values(None))
+#   -> []
