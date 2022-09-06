16031986d6669aec3a2a3719dc9bb471f91721ab
Sajib Mandal <107785806+sajib-mandal@users.noreply.github.com>
2022-09-06 15:19:51 +0530

    Update tree_sum.py

--- Binary Tree/tree_sum.py
+++ Binary Tree/tree_sum.py
@@ -4,6 +4,59 @@ Write a function, tree_sum, that takes in the root of a binary tree that
 contains number values. The function should return the total sum of all
 values in the tree.
 """
+# iterative version breadth_first_search
+
+from collections import deque
+
+
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+
+
+def tree_sum(root):
+    if root is None:
+        return 0
+    total_sum = 0
+    queue = deque([root])
+
+    while queue:
+        current = queue.popleft()
+        total_sum += current.val
+
+        if current.left is not None:
+            queue.append(current.left)
+
+        if current.right is not None:
+            queue.append(current.right)
+    return total_sum
+
+
+a = Node(3)
+b = Node(11)
+c = Node(4)
+d = Node(4)
+e = Node(-2)
+f = Node(1)
+
+a.left = b
+a.right = c
+b.left = d
+b.right = e
+c.right = f
+
+#       3
+#    /    \
+#   11     4
+#  / \      \
+# 4   -2     1
+
+print(tree_sum(a))  # -> 21
+
+
+
 
 # depth_first_search recursive tree sum
 
