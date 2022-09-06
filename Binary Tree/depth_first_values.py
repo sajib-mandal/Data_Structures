1d1f64a3dbcb5896e6c51dc40ae190687ae33864
Sajib Mandal <107785806+sajib-mandal@users.noreply.github.com>
2022-09-06 11:33:41 +0530

    Create depth_first_values.py

---
+++ Binary Tree/depth_first_values.py
@@ -0,0 +1,61 @@
+"""
+Depth First Search:
+Write a function, depth_first_values, that takes in the root of a binary tree.
+The function should return a list containing all values of the tree in depth-first
+order.
+
+Hey. This is our first binary tree problem, so you should be liberal with watching
+the Approach and Walk-through. Be productive, not stubborn. -AZ
+"""
+
+# iterative version
+
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.right = None
+        self.left = None
+
+
+def depth_first_values(root):
+    if root is None:
+        return []
+
+    values = []
+    stack = [root]
+
+    while stack:
+        current = stack.pop()
+        values.append(current.val)
+
+        if current.right is not None:
+            stack.append(current.right)
+        if current.left is not None:
+            stack.append(current.left)
+    return values
+  
+
+a = Node('a')
+b = Node('b')
+c = Node('c')
+d = Node('d')
+e = Node('e')
+f = Node('f')
+a.left = b
+a.right = c
+b.left = d
+b.right = e
+c.right = f
+
+#      a
+#    /   \
+#   b     c
+#  / \     \
+# d   e     f
+
+print(depth_first_values(a))
+#   -> ['a', 'b', 'd', 'e', 'c', 'f']
+
+
+# recursive version
+
