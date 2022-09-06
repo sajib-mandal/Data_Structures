28305a663e44d5eb53e44be804590f1db83398e2
Sajib Mandal <107785806+sajib-mandal@users.noreply.github.com>
2022-09-06 15:24:27 +0530

    Update tree_sum.py

--- Binary Tree/tree_sum.py
+++ Binary Tree/tree_sum.py
@@ -55,7 +55,53 @@ c.right = f
 
 print(tree_sum(a))  # -> 21
 
+# iterative version depth_first_search using stack
 
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+
+
+def tree_sum(root):
+    if root is None:
+        return 0
+    total_sum = 0
+    stack = [root]
+
+    while stack:
+        current = stack.pop()
+        total_sum += current.val
+
+        if current.right is not None:
+            stack.append(current.right)
+
+        if current.left is not None:
+            stack.append(current.left)
+    return total_sum
+
+
+a = Node(3)
+b = Node(11)
+c = Node(4)
+d = Node(4)
+e = Node(-2)
+f = Node(1)
+
+a.left = b
+a.right = c
+b.left = d
+b.right = e
+c.right = f
+
+#       3
+#    /    \
+#   11     4
+#  / \      \
+# 4   -2     1
+
+print(tree_sum(a))  # -> 21
 
 
 # depth_first_search recursive tree sum
