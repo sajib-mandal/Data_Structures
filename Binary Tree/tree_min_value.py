09ab601722b8b2035bd9f29b96f97484439c6e3a
Sajib Mandal <107785806+sajib-mandal@users.noreply.github.com>
2022-09-06 18:33:29 +0530

    Update tree_min_value.py

--- Binary Tree/tree_min_value.py
+++ Binary Tree/tree_min_value.py
@@ -109,3 +109,95 @@ print(tree_min_value(a))  # -> -2
 
 
 # recursive depth_first_search
+
+
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+
+
+def tree_min_value(root):
+    if root is None:
+        return float("inf")
+
+    min_left = tree_min_value(root.left)
+    min_right = tree_min_value(root.right)
+
+    return min(root.val, min_left, min_right)
+
+
+a = Node(3)
+b = Node(11)
+c = Node(4)
+d = Node(4)
+e = Node(-2)
+f = Node(1)
+
+a.left = b
+a.right = c
+b.left = d
+b.right = e
+c.right = f
+
+#       3
+#    /    \
+#   11     4
+#  / \      \
+# 4   -2     1
+print(tree_min_value(a))  # -> -2
+
+a = Node(5)
+b = Node(11)
+c = Node(3)
+d = Node(4)
+e = Node(14)
+f = Node(12)
+
+a.left = b
+a.right = c
+b.left = d
+b.right = e
+c.right = f
+
+#       5
+#    /    \
+#   11     3
+#  / \      \
+# 4   14     12
+
+print(tree_min_value(a))  # -> 3
+
+a = Node(-1)
+b = Node(-6)
+c = Node(-5)
+d = Node(-3)
+e = Node(-4)
+f = Node(-13)
+g = Node(-2)
+h = Node(-2)
+
+a.left = b
+a.right = c
+b.left = d
+b.right = e
+c.right = f
+e.left = g
+f.right = h
+
+#        -1
+#      /   \
+#    -6    -5
+#   /  \     \
+# -3   -4   -13
+#     /       \
+#    -2       -2
+
+print(tree_min_value(a))  # -> -13
+
+a = Node(42)
+
+#        42
+
+print(tree_min_value(a))  # -> 42
