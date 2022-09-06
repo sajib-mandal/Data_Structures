5e40bee73b3a1b955bd4af2e0340766b159af9d4
Sajib Mandal <107785806+sajib-mandal@users.noreply.github.com>
2022-09-06 22:41:06 +0530

    Create right_side_view.py

---
+++ Binary Tree/Coding Problem/right_side_view.py
@@ -0,0 +1,60 @@
+"""
+199. Binary Tree Right Side View:
+Given the root of a binary tree, imagine yourself standing on the right
+side of it, return the values of the nodes you can see ordered from top
+to bottom.
+"""
+
+
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+
+
+def right_side_view(root):
+    if root is None:
+        return []
+    values = []
+    stack = [root]
+
+    while stack:
+        current = stack.pop()
+        values.append(current.val)
+        if current.right is not None:
+            stack.append(current.right)
+
+    return values
+
+
+a = Node(1)
+b = Node(2)
+c = Node(3)
+d = Node(4)
+e = Node(5)
+
+a.left = b
+a.right = c
+b.right = e
+c.right = d
+
+#       1
+#      /  \
+#     2    3
+#      \    \
+#       5    4
+
+print(right_side_view(a))    # [1, 3, 4]
+
+
+a = Node(1)
+b = Node(3)
+a.right = b
+
+#      1
+#       \
+#        3
+print(right_side_view(a))  # [1, 3]
+
+print(right_side_view(None))  # []
