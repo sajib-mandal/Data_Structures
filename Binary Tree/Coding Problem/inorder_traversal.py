527c84d211c9e04cc2ed406c7d52a27740ed66c4
Sajib Mandal <107785806+sajib-mandal@users.noreply.github.com>
2022-09-11 18:35:28 +0530

    Update inorder_traversal.py

--- Binary Tree/Coding Problem/inorder_traversal.py
+++ Binary Tree/Coding Problem/inorder_traversal.py
@@ -76,3 +76,52 @@ b.right = e
 
 print(inorder(a))
 print(inorder(None))
+
+
+
+# terative
+
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+
+
+def inorder(root):
+    if root is None:
+        return []
+
+    values = []
+    stack = []
+    curr = root
+
+    while stack or curr:
+        if curr:
+            stack.append(curr)
+            curr = curr.left
+        else:
+            curr = stack.pop()
+            values.append(curr.val)
+            curr = curr.right
+    return values
+
+
+a = Node(1)
+b = Node(2)
+c = Node(3)
+d = Node(4)
+e = Node(5)
+f = Node(6)
+g = Node(7)
+h = Node(8)
+
+a.left = b
+a.right = c
+b.left = d
+c.left = e
+c.right = f
+e.left = g
+e.right = h
+
+print(inorder(a))
